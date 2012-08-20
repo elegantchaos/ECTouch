45bca7197c263453412bf8c83e5efbf9fb0b5a5f
Sam Deane <sam@elegantchaos.com>
2012-08-20 17:40:52 +0100

    added framework header

---
+++ Source/iOS/ECTouch.h
@@ -0,0 +1,22 @@
+// --------------------------------------------------------------------------
+//  Copyright 2012 Sam Deane, Elegant Chaos. All rights reserved.
+//  This source code is distributed under the terms of Elegant Chaos's 
+//  liberal license: http://www.elegantchaos.com/license/liberal
+// --------------------------------------------------------------------------
+
+#import "ECTAppDelegate.h"
+#import "ECTCoreDataModelController.h"
+#import "ECTModelController.h"
+
+#import "ECTBinding.h"
+#import "ECTBindingViewController.h"
+#import "ECTButtonCell.h"
+#import "ECTEditableCell.h"
+#import "ECTKeys.h"
+#import "ECTSection.h"
+#import "ECTSectionDrivenTableController.h"
+#import "ECTSimpleCell.h"
+#import "ECTSingleTableViewController.h"
+#import "ECTSubtitleCell.h"
+#import "ECTSwitchCell.h"
+#import "ECTTickCell.h"
